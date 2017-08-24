1e3d3ba7ccb22e26e19d45f782b6ea5eab13883f
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-08-24 23:52:33 +0200

    Last touches

--- clock.py
+++ clock.py
@@ -4,9 +4,9 @@ from main import do_job
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=8)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=8)
 def scheduled_job():
-    print('This job is run every monday at 8am.')
+    print('This job is run every day at 8am.')
     do_job()
 
 sched.start()
--- main.py
+++ main.py
@@ -8,34 +8,41 @@ load_dotenv(find_dotenv())
 from coinbase.wallet.client import Client
 from sendgrid.helpers.mail import *
 
-def send_mail(sender, recipient, subject):
+def send_mail(sender, recipient, subject, message):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email(sender[0], sender[1])
     to_email = Email(recipient)
-    content = Content("text/plain", " ")
+    content = Content('text/plain', message)
     mail = Mail(from_email, subject, to_email, content)
-    try:
-        response = sg.client.mail.send.post(request_body=mail.get())
-    except Exception as error:
-        print(f'Email not sent for {recipient}.\nFollowing error has occured:\n{error}')
+    response = sg.client.mail.send.post(request_body=mail.get())
 
 def get_current_gains():
     api_key, api_secret = os.environ.get('API_KEY'), os.environ.get('API_SECRET')
     client = Client(api_key, api_secret)
 
     accounts = client.get_accounts()['data']
-    gains = 0
+    gains, total = {}, 0
     for acc in accounts:
         id, curr, nat_bal = acc['id'], acc['balance']['currency'], float(acc['native_balance']['amount'])
         trans = client.get_transactions(id)['data']
         nat_payments = sum([float(t['native_amount']['amount']) for t in trans])
-        gains += nat_bal - nat_payments
-    return '{:.2f}'.format(gains)
+        gain = nat_bal - nat_payments
+        total += gain
+        gains[curr] = '{:.2f}'.format(gain)
+    total = '{:.2f}'.format(total)
+    return gains, total
 
 def do_job():
-    gains = get_current_gains() or 'N/A'
-    sender = ('no-reply@cb-24613.com', 'Coinbase')
-    subject = f'Your gains in Coinbase are currently: {gains}€'
-    recipient = 'elmehdi.baha@gmail.com'
-    send_mail(sender, recipient, subject)
+    try:
+        # Getting gains
+        gains, total = get_current_gains()
+        # Setting mail
+        sender = ('no-reply@cb-24613.com', 'Crypto Daily')
+        recipient = 'elmehdi.baha@gmail.com'
+        subject = f'Your gains in Coinbase are currently: {total}€'
+        message = '\n'.join([f'Gains in {curr}: {amt}€' for curr, amt in gains.items()])
+        # Sending mail
+        send_mail(sender, recipient, subject, message)
+    except Exception as e:
+        print(f'Following error occured: {e}')
 
