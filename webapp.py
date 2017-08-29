5b92f585279d02d3d3e0a674bfd4bb802fb5ca26
Mehdi B <elmehdi.baha@gmail.com>
2017-08-29 15:25:17 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,10 +12,7 @@ def index():
 def entry_point(user):
     if user not in ['mehdi', 'ali']:
         return 'no user found.'
-    if user == 'mehdi':
-        gains, total = get_current_gains()
-    if user == 'ali':
-        gains, total = {'ETH': '0.0', 'BTC': '0.0'}, '0.0'
+    gains, total = get_current_gains(user)
     s = [f'Your total gains are: {total}€']
     s += [f'Gains in {curr}: {amt}€' for curr, amt in gains.items()]
     return '<br>'.join(s)
