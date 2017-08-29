b28d8f1c791fa182724775e858289c386e1b8da2
Mehdi B <elmehdi.baha@gmail.com>
2017-08-29 14:39:42 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from gains import get_current_gains
 
 app = Flask(__name__)
 
 @app.route('/')
-def entry_point():
-    gains, total = get_current_gains()
+def index():
+    root = request.url.split('/')[0]
+    return f'no user selected. go to {root}/<user>'
+
+@app.route('/<user>')
+def entry_point(user):
+    if user not in ['mehdi', 'ali']:
+        return 'no user found.'
+    if user == 'mehdi':
+        gains, total = get_current_gains()
+    is user == 'ali':
+        gains, total = {'ETH': '0.0', 'BTC', '0.0'}, '0.0'
     s = [f'Your total gains are: {total}€']
     s += [f'Gains in {curr}: {amt}€' for curr, amt in gains.items()]
     return '<br>'.join(s)
