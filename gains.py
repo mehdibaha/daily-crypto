cbd38be948ff1a6c453182845d13978d7f418afd
Mehdi B <elmehdi.baha@gmail.com>
2017-08-31 10:28:00 +0200

    Update gains.py

--- gains.py
+++ gains.py
@@ -4,6 +4,9 @@ import os
 from coinbase.wallet.client import Client
 from coinbase.wallet.model import APIObject
 
+# source: https://support.coinbase.com/customer/portal/articles/2109597-buy-sell-bank-transfer-fees
+SELL_FEE = 0.0149
+
 def get_current_gains(user):
     api_key, api_secret = os.environ.get(f'API_KEY_{user.upper()}'), os.environ.get(f'API_SECRET_{user.upper()}')
     client = Client(api_key, api_secret)
@@ -19,6 +22,7 @@ def get_current_gains(user):
         res = client._make_api_object(client._get('v2', 'prices', f'{curr.upper()}-EUR', 'sell'), APIObject)
         spot_price = float(res.amount)
         nat_bal = sum([float(t['amount']['amount']) * spot_price for t in trans])
+        nat_bal -= SELL_FEE*nat_bal
         nat_payments = sum([float(t['native_amount']['amount']) for t in trans])
         gain = nat_bal - nat_payments
         total += gain
