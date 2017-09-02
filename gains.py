8e2e80eb0fe1fce90b36a8ad197e7806595cec53
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-09-02 10:41:50 +0200

    Add colors for gain and better formating

--- gains.py
+++ gains.py
@@ -10,6 +10,7 @@ SELL_FEE = 0.0149
 def get_current_gains(user):
     api_key, api_secret = os.environ.get(f'API_KEY_{user.upper()}'), os.environ.get(f'API_SECRET_{user.upper()}')
     client = Client(api_key, api_secret)
+    nat_curr = os.environ.get('NAT_CURRENCY', 'EUR')
 
     accounts = client.get_accounts()['data']
     gains, total = {}, 0
@@ -19,14 +20,20 @@ def get_current_gains(user):
             continue
         trans = client.get_transactions(id)['data']
         trans = [t for t in trans if t['type'] == 'buy']
-        res = client._make_api_object(client._get('v2', 'prices', f'{curr.upper()}-EUR', 'sell'), APIObject)
+        res = client._make_api_object(client._get('v2', 'prices', f'{curr.upper()}-{nat_curr}', 'sell'), APIObject)
         spot_price = float(res.amount)
         nat_bal = sum([float(t['amount']['amount']) * spot_price for t in trans])
         nat_bal -= SELL_FEE*nat_bal
         nat_payments = sum([float(t['native_amount']['amount']) for t in trans])
         gain = nat_bal - nat_payments
         total += gain
-        if gain > 0:
-            gains[curr] = '{:.2f}'.format(gain)
-    total = '{:.2f}'.format(total)
+        gains[curr] = gain
     return gains, total
+
+def get_fake_gains(user):
+    gains, total =  {'ETH': -51.23, 'BTC': 53.4, 'LTC': 0.0}, 2.1700001
+    return gains, total
+
+
+
+
--- webapp.py
+++ webapp.py
@@ -1,7 +1,12 @@
+import os
+
 from flask import Flask, render_template, request
-from gains import get_current_gains
+from gains import get_current_gains, get_fake_gains
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+NAT_CURR = 'EUR'
 
 @app.route('/')
 def index():
@@ -11,8 +16,21 @@ def index():
 def entry_point(user):
     if user not in ['mehdi', 'ali']:
         return 'no user found.'
-    gains, total = get_current_gains(user)
+    try:
+        gains, total = get_current_gains(user)
+    except Exception as e:
+        print(f'following error has occured\n{e}')
+        gains, total = get_fake_gains(user)
     return render_template('index.html', gains=gains, total=total)
+
+@app.template_filter('prettify')
+def prettify(amt):
+    currencies = {'USD': '$', 'EUR': '€', 'GBP': '£'}
+    nat_curr = os.environ.get('NAT_CURRENCY', 'EUR')
+    fmt = '+' if amt > 0 else '' # Adding value sign
+    fmt += '{:.2f}'.format(amt) # Setting float precision
+    fmt += currencies[nat_curr] # Add currency symbol
+    return fmt
 
 if __name__ == '__main__':
     app.run(debug=True)
